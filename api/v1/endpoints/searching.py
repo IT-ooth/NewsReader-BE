92cbe20308b9db0471d825384b2fb4c79078f97e
Jeong Yeon Su <57950931+sunshinetoyou@users.noreply.github.com>
2026-01-11 00:32:44 +0900

    theme-list 반환 로직 추가

--- api/v1/endpoints/searching.py
+++ api/v1/endpoints/searching.py
@@ -3,11 +3,11 @@ from sqlmodel import Session
 
 from db.connection import get_session
 from db.models import ThemeSearchRequest
-from db.services import search_articles_by_all_themes, search_articles_by_any_themes
+from db.services import search_articles_by_all_themes, search_articles_by_any_themes, get_active_themes
 
 router = APIRouter()
 
-@router.post("/articles/search/theme")
+@router.post("/search/theme")
 def search_any_themes(
     req: ThemeSearchRequest,
     session: Session = Depends(get_session)
@@ -25,3 +25,10 @@ def search_any_themes(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Invalid api_type. Use 1 for ANY or 2 for ALL."
         )
+
+@router.post("/theme/list")
+def get_theme_list(
+    session: Session = Depends(get_session)
+):
+    """DB내에 존재하는 테마 리스트 반환 로직"""
+    return get_active_themes(session)
